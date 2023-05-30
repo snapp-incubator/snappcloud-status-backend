619ba6c4dd6f5217cdb4a96263d22753d99c3508
mohammadne <mohammadne.dev@gmail.com>
2023-05-30 09:05:54 +0330

    feat: update values

--- deployments/tearup.sh
+++ deployments/tearup.sh
@@ -3,18 +3,15 @@
 set -e
 
 cmd=$1
-override=$2
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-path_to_k8s="$current_dir/$cmd"
+path_to_helm="$current_dir/$cmd"
 
-if [ ! -d "$path_to_k8s" ]; then
+if [ ! -d "$path_to_helm" ]; then
     echo "Error: command (directory) $1 does not exists."
     exit 9999 # die with error code 9999
 fi
 
 echo "tearup snappcloud-status-backend-$cmd ..."
 
-helm upgrade --install "snappcloud-status-backend-$cmd" "$path_to_k8s" \
-	-f "$path_to_k8s/values.yaml" \
-	-f "$path_to_k8s/overrides/$override.yaml"
+helm upgrade --install "snappcloud-status-backend-$cmd" "$path_to_helm"
